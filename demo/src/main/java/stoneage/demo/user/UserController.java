d150bad05168b2657307a22009d87042c26534ba
JosephRidge <josephridge18@gmail.com>
2022-09-25 13:01:05 +0300

    Get User by Name success call

--- demo/src/main/java/stoneage/demo/user/UserController.java
+++ demo/src/main/java/stoneage/demo/user/UserController.java
@@ -1,7 +1,11 @@
 package stoneage.demo.user;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +26,63 @@ public class UserController {
      * with Transactional
      * 
      */
-    @GetMapping // set the first GET verb
-    public User getUser() {
-        return new User();
+    
+    // @GetMapping 
+    // public void getUser(@RequestBody String name) {
+    //     try {
+    //         userService.getUser(name);
+    //     } catch (InterruptedException e) { 
+    //         e.printStackTrace();
+    //     } catch (ExecutionException e) { 
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+       try {
+        return userService.getAllUsers();
+    } catch (InterruptedException e) { 
+        e.printStackTrace();
+        return null;
+    } catch (ExecutionException e) { 
+        e.printStackTrace();
+        return null;
     }
+    }
+
+    @GetMapping("{username}") 
+    public void getUserDetails(@PathVariable String username){ // map the user request body
+        try {
+            userService.getUser(username);
+        } catch (InterruptedException e) { 
+            e.printStackTrace();
+        } catch (ExecutionException e) { 
+            e.printStackTrace();
+        }
+    } 
 
     @PostMapping
     public void saveNewUser(@RequestBody User user) { // map the user request body
         userService.addNewUser(user);
     }
+
+    // @PostMapping
+    // public void getUserDetails(@RequestBody String name) { // map the user request body
+    //     try {
+    //         userService.getUser(name);
+    //     } catch (InterruptedException e) { 
+    //         e.printStackTrace();
+    //     } catch (ExecutionException e) { 
+    //         e.printStackTrace();
+    //     }
+    // }
+ 
+
+
+    // @GetMapping
+    // public void getAllUsers(){ 
+    //         userService.getAllUsers(); 
+    // }
+
 }
--- demo/src/main/java/stoneage/demo/user/UserService.java
+++ demo/src/main/java/stoneage/demo/user/UserService.java
@@ -1,34 +1,75 @@
 package stoneage.demo.user;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.stereotype.Service;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 
 @Service
 public class UserService {
 
-    // Also works as user details updater
-    
+    // Also works as user details update
     public String addNewUser(User user) {
-        // get firestore instance 
-        Firestore dbFireStore = FirestoreClient.getFirestore();
-        
-        //get firestore collectionA
-        ApiFuture<WriteResult> collectionApiFuture = dbFireStore.collection("user").
-        document(user.getUsername()).set(user);
+        Firestore dbFireStore = FirestoreClient.getFirestore(); // get firestore instance
+        ApiFuture<WriteResult> collectionApiFuture = dbFireStore.collection("user").document(user.getUsername())
+                .set(user);// get firestore collectionA
         try {
             return collectionApiFuture.get().getUpdateTime().toString();
-        } catch (InterruptedException e) { 
+        } catch (InterruptedException e) {
             e.printStackTrace();
             return e.getMessage();
-        } catch (ExecutionException e) { 
+        } catch (ExecutionException e) {
             e.printStackTrace();
             return e.getMessage();
         }
+    }
+
+    // get praticular user
+    public User getUser(String name)
+            throws InterruptedException, ExecutionException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();// get firestore instance
+        DocumentReference docRef = dbFireStore.collection("user").document(name);
+        ApiFuture<DocumentSnapshot> future = docRef.get(); // asynchronously retrieve the document
+        DocumentSnapshot document = future.get();// future.get() blocks on response
+        User user = null;
+        if (document.exists()) {
+            System.out.println("Document data: " + document.getData());
+            user = document.toObject(User.class);
+
+            return user;
+        } else {
+            System.out.println("No such document!");
+            return null;
+        }
+
+    }
+
+    /**
+     * Get All documents
+     * 
+     */
+    public List<User> getAllUsers()
+            throws InterruptedException, ExecutionException {
+        List<User>users = null;
+        Firestore dbFireStore = FirestoreClient.getFirestore();// get firestore instance
+        ApiFuture<QuerySnapshot> future = dbFireStore.collection("user").get();  // asynchronously retrieve all documents
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments(); // future.get() blocks on response
+        
+        User user = null;
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println(document.getId() + " => " + document.toObject(User.class));
+            user = document.toObject(User.class);
+            users.add(user);
+        }
+        return users;
     }
 }
